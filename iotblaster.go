84748dacb9b74e4367da56f0b24e66dbff7a8308
GrandArcher <andrewe02000@gmail.com>
2018-10-20 13:00:20 -0400

    Learning how to ping things.

--- iotblaster.go
+++ iotblaster.go
@@ -3,4 +3,13 @@
 package main
 
 func main() {
+	pinger, err := ping.NewPinger("www.google.com")
+	if err != nil {
+		panic(err)
+	}
+
+	pinger.Count = 3
+	pinger.Run()                 // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+
 }
